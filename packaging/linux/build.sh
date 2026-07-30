bc3f85cde1c69e317a85266bbfe17c2fe66857b4
Sdata <sdata.r@qq.com>
2026-07-30 22:09:07 +0800

    fix: preserve prebuilt binaries in RPMs

--- packaging/linux/build.sh
+++ packaging/linux/build.sh
@@ -412,6 +412,7 @@ EOF
   mkdir -p "$top"/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
   rpmbuild -bb "$spec" \
     --target "$rpm_arch" \
+    --define "__strip /bin/true" \
     --define "_topdir $top" \
     --define "_payload $root"
   rpm_file=$(find "$top/RPMS" -type f -name '*.rpm' -print -quit)
